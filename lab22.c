dedf99ccb8bd25953f5051e32089939ef6ece0ef
kennethsample <kenneth.sample@afacademy.af.edu>
2022-10-11 17:20:06 -0600

    updated to pass num_games to every function

--- lab22.c
+++ lab22.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
-#define NUM_GAMES 10
-
-/** 
- * @brief normalizes a value to a new range - for example, if you normalized a percentage of 0.67 to a range between 0 and 20 it would return 13.4.
- * @param valueToNormalize the value you want to normalize
- * @param initialRangeMin the minimum value for the range you want to normalize, for example if the value is currently a percentage then this would be 0.0.
- * @param initialRangeMax the minimum value for the range you want to normalize, for example if the value is currently a percentage then this would be 1.0 (or 100.0).
- * @param normalizedMin the minimum value for the new range you are normalizing to.  for example, if you want the normalized values to be between 3.0 and 5.0, this would be 3.0
- * @param normalizedMax the maximum value for the new range you are normalizing to.  for example, if you want the normalized values to be between 3.0 and 5.0, this would be 5.0
- * @return the new normalized value
- */ 
-double normalizeValue(double valueToNormalize, double initialRangeMin, double initialRangeMax, double normalizedMin, double normalizedMax){
-    return (valueToNormalize - initialRangeMin)/(initialRangeMax - initialRangeMin)*(normalizedMax-normalizedMin) + normalizedMin;
-}
+#define MAX_GAMES = 1075
 
 /** 
  * @brief combine field goal percentage 3-pt percentage, and free throw percentage into one shooting stat
@@ -32,10 +19,11 @@ double getShootingEffectivenessFromAverages(double fgPct, double fg3Pct, double 
  * @param awayIDs the awayIDs for all games
  * @param homeScores the ptsHome values for all games
  * @param awayScores the ptsAway values for all games
+ * @param num_games the number of games being analyzed (the length of the parallel arrays)
  * @return the point differential per game across all game for the team
  * The differential will be positive if the team has gotten more points per game than their opponents, and vice versa
  */ 
-double pointDifferentialPerGameOneTeam(int teamID, int* homeIDs, int* awayIDs, int* homeScores, int* awayScores){
+double pointDifferentialPerGameOneTeam(int teamID, int* homeIDs, int* awayIDs, int* homeScores, int* awayScores, int num_games){
     
     return 0.0;
 }
@@ -47,10 +35,11 @@ double pointDifferentialPerGameOneTeam(int teamID, int* homeIDs, int* awayIDs, i
  * @param homeIDs the homeIDs for all games
  * @param awayIDs the awayIDs for all games
  * @param gameResults results of all games, 1 if home team won, and 0 otherwise
+ * @param num_games the number of games being analyzed (the length of the parallel arrays)
  * @return the win/loss differential - The differential will be positive if the home team has won more, and vice versa.
  * For example, if the home team has won 2 more games, then return 2.  if the away team has won 1 more game then return -1.
  */ 
-int headToHeadWL(int homeID, int awayID, int* homeIDs, int* awayIDs, int* gameResults){
+int headToHeadWL(int homeID, int awayID, int* homeIDs, int* awayIDs, int* gameResults, int num_games){
     
     return 0;
 }
@@ -66,10 +55,11 @@ int headToHeadWL(int homeID, int awayID, int* homeIDs, int* awayIDs, int* gameRe
  * @param awayFg3Pcts the 3-point field goal percentages for the away teams for all games
  * @param homeFtPcts the free throw percentages for the home teams for all games
  * @param awayFtPcts the free throw percentages for the away teams for all games
+ * @param num_games the number of games being analyzed (the length of the parallel arrays)
  * @return the shooting effectiveness of the team in question - 
  * The return value should be the result of the function getShootingEffectivenessFromAverages() given the team's fgPct, fg3Pct, and ftPct across all games
  */ 
-double shootingEffectivenessOneTeam(int teamID, int* homeIDs, int* awayIDs, double* homeFgPcts, double* awayFgPcts, double* homeFg3Pcts, double* awayFg3Pcts, double* homeFtPcts, double* awayFtPcts){
+double shootingEffectivenessOneTeam(int teamID, int* homeIDs, int* awayIDs, double* homeFgPcts, double* awayFgPcts, double* homeFg3Pcts, double* awayFg3Pcts, double* homeFtPcts, double* awayFtPcts, int num_games){
     
     return 0.0;
 }
@@ -82,10 +72,11 @@ double shootingEffectivenessOneTeam(int teamID, int* homeIDs, int* awayIDs, doub
  * @param awayIDs the awayIDs for all games
  * @param homeRebounds the homeRebound values for all games
  * @param awayRebounds the awayRebound values for all games
+ * @param num_games the number of games being analyzed (the length of the parallel arrays)
  * @return the difference in rebounds per game between the home and away team
  * The differential will be positive if the home team averages more rebounds per game, and vice versa
  */ 
-double reboundsPerGameComparison(int homeID, int awayID, int* homeIDs, int* awayIDs, int* homeRebounds, int* awayRebounds){
+double reboundsPerGameComparison(int homeID, int awayID, int* homeIDs, int* awayIDs, int* homeRebounds, int* awayRebounds, int num_games){
     
     return 0.0;
 }
@@ -98,28 +89,14 @@ double reboundsPerGameComparison(int homeID, int awayID, int* homeIDs, int* away
  * @param awayIDs the awayIDs for all games
  * @param homeAssists the homeAssist values for all games
  * @param awayAssists the awayAssist values for all games
+ * @param num_games the number of games being analyzed (the length of the parallel arrays)
  * @return the difference in assists per game between the home and away team
  * The differential will be positive if the home team averages more assists per game, and vice versa
  */ 
-double assistsPerGameComparison(int homeID, int awayID, int* homeIDs, int* awayIDs, int* homeAssists, int* awayAssists){
+double assistsPerGameComparison(int homeID, int awayID, int* homeIDs, int* awayIDs, int* homeAssists, int* awayAssists, int num_games){
     
     return 0.0;
 }
-
-/** 
- * @brief takes the 5 calculated comparison values, normalizes them to ranges between -10.0 and 10.0, and adds them together plus 50 to return a final comparison 
- * @param pointDiffComparison the comparison in point differential between the two teams
- * @param h2hResult the result of head-to-head games between the two teams
- * @param shootingDifferential the shooting differential between the two teams
- * @param reboundDifferential the differential in rebounds per game between the two teams
- * @param assistDifferential the differential in assists per game between the two teams
- * @return the overall normalized comparison (which will range from 0.0 to 100.0)
- */ 
-double teamComparison(double pointDiffComparison, int h2hResult, double shootingDifferential, double reboundDifferential, double assistDifferential){
-    
-    return 0.0;
-}
-
 
 int main(){
 
@@ -127,23 +104,26 @@ int main(){
     int homeID = 48;
     int awayID = 39;
 
+    //For now, you'll test your function using the 10 games listed below.  This will need to change to 1075 when you read from the file.
+    const int num_games = 10;
+
     // These parallel arrays contain SOME game information to test your function
     // Later the lab, you will replace this with the contents of the file games.csv
-    int homeIDs[NUM_GAMES]        = {48,    37,    38,    48,    45,    38,    51,    61,    59,    39};
-    int awayIDs[NUM_GAMES]        = {50,    46,    65,    39,    42,    63,    48,    65,    58,    66};
-    int homeScores[NUM_GAMES]     = {104,   112,   114,   117,   100,   120,   107,   106,   112,   98};
-    int awayScores[NUM_GAMES]     = {113,   106,   103,   105,   113,   107,   113,   108,   115,   119};
-    int homeAssists[NUM_GAMES]    = {23,    28,    23,    28,    29,    33,    24,    12,    28,    21};
-    int awayAssists[NUM_GAMES]    = {21,    22,    21,    25,    20,    24,    27,    25,    26,    32};
-    int homeRebounds[NUM_GAMES]   = {53,    47,    47,    42,    39,    51,    30,    41,    40,    37};
-    int awayRebounds[NUM_GAMES]   = {46,    36,    42,    40,    47,    44,    35,    42,    49,    45};
-    int homeTeamWins[NUM_GAMES]   = {0,     1,     1,     1,     0,     1,     0,     0,     0,     0};
-    double homeFgPcts[NUM_GAMES]  = {0.398, 0.478, 0.467, 0.477, 0.477, 0.517, 0.47,  0.434, 0.495, 0.468};
-    double awayFgPcts[NUM_GAMES]  = {0.422, 0.488, 0.422, 0.443, 0.455, 0.424, 0.519, 0.452, 0.459, 0.506};
-    double homeFg3Pcts[NUM_GAMES] = {0.333, 0.29,  0.188, 0.314, 0.2,   0.432, 0.387, 0.269, 0.375, 0.323};
-    double awayFg3Pcts[NUM_GAMES] = {0.357, 0.375, 0.294, 0.32,  0.306, 0.325, 0.333, 0.297, 0.313, 0.447};
-    double homeFtPcts[NUM_GAMES]  = {0.76,  0.895, 0.8,   0.889, 0.632, 0.8,   0.773, 0.75,  0.5,   0.667};
-    double awayFtPcts[NUM_GAMES]  = {0.875, 0.824, 0.958, 0.931, 0.786, 0.769, 0.821, 0.75,  0.844, 0.727};
+    int homeIDs[10]        = {48,    37,    38,    48,    45,    38,    51,    61,    59,    39};
+    int awayIDs[10]        = {50,    46,    65,    39,    42,    63,    48,    65,    58,    66};
+    int homeScores[10]     = {104,   112,   114,   117,   100,   120,   107,   106,   112,   98};
+    int awayScores[10]     = {113,   106,   103,   105,   113,   107,   113,   108,   115,   119};
+    int homeAssists[10]    = {23,    28,    23,    28,    29,    33,    24,    12,    28,    21};
+    int awayAssists[10]    = {21,    22,    21,    25,    20,    24,    27,    25,    26,    32};
+    int homeRebounds[10]   = {53,    47,    47,    42,    39,    51,    30,    41,    40,    37};
+    int awayRebounds[10]   = {46,    36,    42,    40,    47,    44,    35,    42,    49,    45};
+    int homeTeamWins[10]   = {0,     1,     1,     1,     0,     1,     0,     0,     0,     0};
+    double homeFgPcts[10]  = {0.398, 0.478, 0.467, 0.477, 0.477, 0.517, 0.47,  0.434, 0.495, 0.468};
+    double awayFgPcts[10]  = {0.422, 0.488, 0.422, 0.443, 0.455, 0.424, 0.519, 0.452, 0.459, 0.506};
+    double homeFg3Pcts[10] = {0.333, 0.29,  0.188, 0.314, 0.2,   0.432, 0.387, 0.269, 0.375, 0.323};
+    double awayFg3Pcts[10] = {0.357, 0.375, 0.294, 0.32,  0.306, 0.325, 0.333, 0.297, 0.313, 0.447};
+    double homeFtPcts[10]  = {0.76,  0.895, 0.8,   0.889, 0.632, 0.8,   0.773, 0.75,  0.5,   0.667};
+    double awayFtPcts[10]  = {0.875, 0.824, 0.958, 0.931, 0.786, 0.769, 0.821, 0.75,  0.844, 0.727};
 
 
     // ----------------------------------------------------------------------------------
