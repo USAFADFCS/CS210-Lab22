18f2b57e63ecbd9c33bcbe45d8e39ff8eded8f63
kennethsample <kennethsample@hotmail.com>
2022-10-17 07:06:01 -0600

    Update lab22.h

--- lab22.h
+++ lab22.h
@@ -16,16 +16,16 @@ double pointDifferentialPerGameOneTeam(int teamID, int* homeIDs, int* awayIDs, i
 
 /** 
  * @brief compare who won when the two teams we care about played each other previously
- * @param homeID the home team for the game being analyzed
- * @param awayID the away team for the game being analyzed
+ * @param team1 the ID for team1 for the game being analyzed
+ * @param team2 the ID for team2 for the game being analyzed
  * @param homeIDs the homeIDs for all games
  * @param awayIDs the awayIDs for all games
  * @param gameResults results of all games, 1 if home team won, and 0 otherwise
  * @param num_games the number of games being analyzed (the length of the parallel arrays)
- * @return the win/loss differential - The differential will be positive if the home team has won more, and vice versa.
- * For example, if the home team has won 2 more games, then return 2.  if the away team has won 1 more game then return -1.
+ * @return the win/loss differential - The differential will be positive if the team 1 has won more, and vice versa.
+ * For example, if team 1 has won 2 more games, then return 2.  if the team 2 has won 1 more game then return -1.
  */ 
-int headToHeadWL(int homeID, int awayID, int* homeIDs, int* awayIDs, int* gameResults, int num_games);
+int headToHeadWL(int team1, int team2, int* homeIDs, int* awayIDs, int* gameResults, int num_games);
 
 /** 
  * @brief get the shooting effectiveness of a team - this stat combines averages of field goal percentage, 3-point percentage, and free throw percentage
@@ -46,30 +46,30 @@ double shootingEffectivenessOneTeam(int teamID, int* homeIDs, int* awayIDs, doub
 
 /** 
  * @brief compare rebounds per game for two teams
- * @param homeID the home team for the game being analyzed
- * @param awayID the away team for the game being analyzed
+ * @param team1 the ID for team1 for the game being analyzed
+ * @param team2 the ID for team2 for the game being analyzed
  * @param homeIDs the homeIDs for all games
  * @param awayIDs the awayIDs for all games
  * @param homeRebounds the homeRebound values for all games
  * @param awayRebounds the awayRebound values for all games
  * @param num_games the number of games being analyzed (the length of the parallel arrays)
  * @return the difference in rebounds per game between the home and away team
- * The differential will be positive if the home team averages more rebounds per game, and vice versa
+ * The differential will be positive if team 1 averages more rebounds per game, and vice versa
  */ 
-double reboundsPerGameComparison(int homeID, int awayID, int* homeIDs, int* awayIDs, int* homeRebounds, int* awayRebounds, int num_games);
+double reboundsPerGameComparison(int team1, int team2, int* homeIDs, int* awayIDs, int* homeRebounds, int* awayRebounds, int num_games);
 
 /** 
  * @brief compare assists per game for two teams
- * @param homeID the home team for the game being analyzed
- * @param awayID the away team for the game being analyzed
+ * @param team1 the ID for team1 for the game being analyzed
+ * @param team2 the ID for team2 for the game being analyzed
  * @param homeIDs the homeIDs for all games
  * @param awayIDs the awayIDs for all games
  * @param homeAssists the homeAssist values for all games
  * @param awayAssists the awayAssist values for all games
  * @param num_games the number of games being analyzed (the length of the parallel arrays)
- * @return the difference in assists per game between the home and away team
- * The differential will be positive if the home team averages more assists per game, and vice versa
+ * @return the difference in assists per game between team 1 and team 2
+ * The differential will be positive if team 1 averages more assists per game, and vice versa
  */ 
-double assistsPerGameComparison(int homeID, int awayID, int* homeIDs, int* awayIDs, int* homeAssists, int* awayAssists, int num_games);
+double assistsPerGameComparison(int team1, int team2, int* homeIDs, int* awayIDs, int* homeAssists, int* awayAssists, int num_games);
 
 #endif
