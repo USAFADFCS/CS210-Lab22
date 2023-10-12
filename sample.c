b28b7282246dd1c9d29c46098ec14ca9c62b5d7a
kennethsample <kenneth.sample@afacademy.af.edu>
2023-10-12 10:00:51 -0600

    M3 push

--- sample.c
+++ sample.c
@@ -10,6 +10,20 @@
  * The differential will be positive if the team has gotten more points per game than their opponents, and vice versa
  */ 
 double pointDifferentialPerGameOneTeam(int teamID, int* homeIDs, int* awayIDs, int* homeScores, int* awayScores, int num_games){
-    
-    return 0.0;
+    int pointDiff = 0;
+    int gamesCounted = 0;
+
+    for(int i=0;i<num_games;i++){
+        if (teamID == homeIDs[i]){
+            pointDiff += (homeScores[i]-awayScores[i]);
+            gamesCounted++;
+        }else if (teamID == awayIDs[i]){
+            pointDiff += (awayScores[i]-homeScores[i]);
+            gamesCounted++;
+        }
+    }
+
+    double diffPerGame = (double)pointDiff/(double)gamesCounted;
+
+    return diffPerGame;
 }
