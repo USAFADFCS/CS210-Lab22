849b75f67521c19e90626d6f24c194ee6041407b
kennethsample <kenneth.sample@afacademy.af.edu>
2023-10-13 09:20:11 -0600

    T3 reset

--- sample.c
+++ sample.c
@@ -11,24 +11,5 @@
  */ 
 double pointDifferentialPerGameOneTeam(int teamID, int* homeIDs, int* awayIDs, int* homeScores, int* awayScores, int num_games){
     
-    int pointDifferentialSum = 0;
-    int numGamesPlayed = 0;
-
-    for (int i = 0; i<num_games;i++){
-        if (teamID == homeIDs[i]){
-            int pointDiff = homeScores[i]-awayScores[i];
-            pointDifferentialSum+=pointDiff;
-            numGamesPlayed++;
-        }
-        if (teamID == awayIDs[i]){
-            int pointDiff = awayScores[i]-homeScores[i];
-            pointDifferentialSum+=pointDiff;
-            numGamesPlayed++;
-        }
-    }
-
-    double average = (double)pointDifferentialSum/numGamesPlayed;
-
-
-    return average;
+    return 0.0;
 }
