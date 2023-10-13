e74de44778baf4910afe878f7156b83f31613ad4
kennethsample <kenneth.sample@afacademy.af.edu>
2023-10-13 11:31:07 -0600

    T6 reset

--- jameson.c
+++ jameson.c
@@ -11,36 +11,6 @@
  * The differential will be positive if team 1 averages more rebounds per game, and vice versa
  */ 
 double reboundsPerGameComparison(int team1, int team2, int* homeIDs, int* awayIDs, int* homeRebounds, int* awayRebounds, int num_games){
-    double avg1Rbds = 0;
-    double avg2Rbds = 0;
-    int sum1Rbds = 0;
-    int sum2Rbds = 0;
-    int num1Games = 0;
-    int num2Games = 0;
-
-    for (int i = 0; i < num_games; i++){
-        if (team1 == homeIDs[i]){
-            sum1Rbds += homeRebounds[i];
-            num1Games++;
-        }
-        else if(team1 == awayIDs[i]){
-            sum1Rbds += awayRebounds[i];
-            num1Games++;
-        }
-        if (team2 == homeIDs[i]){
-            sum2Rbds += homeRebounds[i];
-            num2Games++;
-        }
-        else if(team2 == awayIDs[i]){
-            sum2Rbds += awayRebounds[i];
-            num2Games++;
-        }
-
-    }
-
-    avg1Rbds = (double) sum1Rbds / (double) num1Games;
-    avg2Rbds = (double) sum2Rbds / (double) num2Games;
     
-    //diff avg rbds
-    return avg1Rbds - avg2Rbds;
+    return 0.0;
 }
--- sample.c
+++ sample.c
@@ -11,23 +11,5 @@
  */ 
 double pointDifferentialPerGameOneTeam(int teamID, int* homeIDs, int* awayIDs, int* homeScores, int* awayScores, int num_games){
     
-    int pointDiffSum = 0;
-    int numGamesOurTeamHasPlayedIn = 0;
-
-    for(int i = 0;i<num_games;i++){
-        if(teamID == homeIDs[i]){
-            int pointDiff = homeScores[i] - awayScores[i];
-            pointDiffSum+=pointDiff;
-            numGamesOurTeamHasPlayedIn++;
-        }else if(teamID == awayIDs[i]){
-            int pointDiff = awayScores[i] - homeScores[i];
-            pointDiffSum+=pointDiff;
-            numGamesOurTeamHasPlayedIn++;
-        }
-
-    }
-
-    double avg = (double)pointDiffSum/numGamesOurTeamHasPlayedIn;
-
-    return avg;
+    return 0.0;
 }
