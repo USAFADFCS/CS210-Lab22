5a84cf899cfbba475eab5ce4188588bb701877d4
troywein <45960380+troywein@users.noreply.github.com>
2024-09-11 10:34:12 -0600

    part 1 of lab completed

--- teamB.c
+++ teamB.c
@@ -10,6 +10,25 @@
  * For example, if team 1 has won 2 more games, then return 2.  if the team 2 has won 1 more game then return -1.
  */ 
 int headToHeadWL(int team1, int team2, int* homeIDs, int* awayIDs, int* gameResults, int num_games){
-    
-    return 0;
+    int wlCounter = 0;
+
+    //Go through each game and check whether it was these two exact teams.  If so, check who won and adjust wlCounter acccordingly
+    //Two if statements are needed to check whether it was these two teams because it counts regardless of who was home or away.
+    for(int i=0;i<num_games;i++){
+        if (team1 == homeIDs[i] && team2 == awayIDs[i]){
+            if (gameResults[i] == 1){
+                wlCounter+=1;
+            }else{
+                wlCounter-=1;
+            }
+        }else if (team2 == homeIDs[i] && team1 == awayIDs[i]){
+            if (gameResults[i] == 1){
+                wlCounter-=1;
+            }else{
+                wlCounter+=1;
+            }
+        }
+    }
+
+    return wlCounter;
 }
