30c86f58b30179a26ce3714cc8a223325904a30e
troywein <troywein@gmail.com>
2024-10-16 21:12:33 -0600

    updates for f25

--- lab22.h
+++ lab22.h
@@ -27,12 +27,12 @@ double pointDifferentialPerGameOneTeam(int teamID, int* homeIDs, int* awayIDs, i
  * @param team2 the ID for team2 for the game being analyzed
  * @param homeIDs the homeIDs for all games
  * @param awayIDs the awayIDs for all games
- * @param gameResults results of all games, 1 if home team won, and 0 otherwise
+ * @param homeTeamWins results of all games, 1 if home team won, and 0 otherwise
  * @param num_games the number of games being analyzed (the length of the parallel arrays)
  * @return the win/loss differential - The differential will be positive if the team 1 has won more, and vice versa.
  * For example, if team 1 has won 2 more games, then return 2.  if the team 2 has won 1 more game then return -1.
  */ 
-int headToHeadWL(int team1, int team2, int* homeIDs, int* awayIDs, int* gameResults, int num_games);
+int headToHeadWL(int team1, int team2, int* homeIDs, int* awayIDs, int* homeTeamWins, int num_games);
 
 /** 
  * @brief get the shooting effectiveness of a team - this stat combines averages of field goal percentage, 3-point percentage, and free throw percentage
